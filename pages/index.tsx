7ecf8b3de765efe5a3441059046cdd3ba02d0391
Phan Thanh Tuan <tuan.phanth91@gmail.com>
2020-10-03 00:11:52 +0700

    init commit
    Build next react app from scratch

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+const IndexPage: React.FC<{}> = () => {
+  return (
+    <>
+      <Head>
+        <meta name="robots" content="TuanPhan-Minshop" />
+      </Head>
+      <div>Hello next js</div>
+    </>
+  );
+};
+
+export default IndexPage;
